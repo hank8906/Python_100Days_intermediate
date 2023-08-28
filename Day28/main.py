250966049414fea7806aa88620f2a90e9d85cd7d
hankhuang <huanghank2000@gmail.com>
2023-08-28 19:50:08 +0800

    Pomodoro

--- Day28/main.py
+++ Day28/main.py
@@ -10,17 +10,34 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def time_reset():
-
-    count_down(5)
+    canvas.itemconfig(time_text, text="00:00")
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_count_down():
-    count_down(5*60)
+    global reps
+    reps += 1
+
+    work_min = WORK_MIN * 60
+    short_break_min = SHORT_BREAK_MIN * 60
+    long_break_min = LONG_BREAK_MIN * 60
+
+    if reps % 2 == 1:
+        count_down(work_min)
+        timer.config(text="Work")
+
+    elif reps % 2 == 0:
+        count_down(short_break_min)
+        timer.config(text="Short break", fg=RED)
+
+    elif reps == 8:
+        count_down(long_break_min)
+        timer.config(text="Long break", fg=RED)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
@@ -29,9 +46,18 @@ def count_down(count):
     minute = math.floor(count / 60)
     second = count % 60
 
+    # change 0 to 00 and 9 to 09
+    if second == 0:
+        second = "00"
+    elif 0 < second < 10:
+        second = "0" + f"{second}"
+
+    # show count down on the canvas
     canvas.itemconfig(time_text, text=f"{minute}:{second}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_count_down()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -44,7 +70,6 @@ tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
 time_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
-
 
 # Timer Label
 timer = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, "bold"), highlightthickness=0, bg=YELLOW)
@@ -59,7 +84,7 @@ check.grid(row=3, column=1)
 start_button = Button(text="Start", command=start_count_down)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset",command=time_reset)
+reset_button = Button(text="Reset", command=time_reset)
 reset_button.grid(row=2, column=2)
 
 window.mainloop()
