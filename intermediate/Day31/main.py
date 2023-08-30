b85cbc24b057c5aae521d1268540f2d62e659164
hankhuang <huanghank2000@gmail.com>
2023-08-31 00:53:12 +0800

    Flash Card

--- intermediate/Day31/main.py
+++ intermediate/Day31/main.py
@@ -1,6 +1,24 @@
 from tkinter import *
+import pandas as pd
+import random
+
 BACKGROUND_COLOR = "#B1DDC6"
-FONT_NAME = "Ariel"
+FONT_NAME = "Arial"
+
+# ---------------------------read data------------------------------------ #
+df = pd.read_csv("./data/french_words.csv")
+word_list = df.to_dict(orient="records")
+
+# ---------------------------random_pick------------------------------------ #
+
+
+def random_pick():
+    random_word = random.choice(word_list)['French']
+    canvas.itemconfigure(title_text, text="French")
+    canvas.itemconfigure(word_text, text=random_word)
+
+
+# ---------------------------backward------------------------------------ #
 
 window = Tk()
 window.title("Flashy")
@@ -12,25 +30,22 @@ canvas.create_image(400, 263, image=card_image)
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Title Label
-title_text = Label(text="Title", bg="white")
-title_text.config(padx=5, pady=5, font=(FONT_NAME, 40, "italic"))
-title_text.place(x=335, y=150)
+title_text = canvas.create_text(400, 150, text="Title", font=(FONT_NAME, 40, "bold"))
 
 # Word Label
-word_text = Label(text="word", bg="white")
-word_text.config(padx=5, pady=5, font=(FONT_NAME, 60, "bold"))
-word_text.place(x=295, y=263)
+initial_word = random.choice(word_list)['French']
+word_text = canvas.create_text(400, 263, text=initial_word, font=(FONT_NAME, 60, "bold"))
 
 # right button
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=random_pick)
 right_button.grid(row=1, column=1)
 
 # wrong button
 wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=random_pick)
 wrong_button.grid(row=1, column=0)
 
-
+random_pick()
 
 window.mainloop()
