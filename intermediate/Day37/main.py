5b3091d2ff7c05b7c3c150833901d688e515ecfe
hankhuang <huanghank2000@gmail.com>
2023-09-04 22:59:04 +0800

    Pixela-Habit Tracking

--- intermediate/Day37/main.py
+++ intermediate/Day37/main.py
@@ -1,20 +1,56 @@
 import requests
 
+# --------------------------------Create a user-------------------------------- #
 user_endpoints = 'https://pixe.la/v1/users'
+USERNAME = "hankhuang"
+TOKEN = "qwertqwertqwert"
+GRAPH_ID = "graph1"
 
-parameters = {
-    "token": "qwertqwertqwert",
-    "username": "hankhuang",
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
+# parameters = {
+#     "token": TOKEN,
+#     "username": USERNAME,
+#     "agreeTermsOfService": "yes",
+#     "notMinor": "yes",
+# }
+#
+# response = requests.post(url=user_endpoints, json=parameters )
+# print(response.text)
+
+# ----------------------------------Create a graph------------------------------ #
+# graph_endpoints = f"{user_endpoints}/{USERNAME}/graphs"
+#
+# headers = {
+#     "X-USER-TOKEN":TOKEN
+# }
+#
+# graph_config = {
+#     "id": "graph1",
+#     "name": "python100",
+#     "unit": "hour",
+#     "type": "float",
+#     "color": "ajisai"
+# }
+#
+# response = requests.post(url=graph_endpoints, headers=headers, json=graph_config)
+# print(response.text)
+
+# --------------------------------POST value to graph---------------------------- #
+
+graph_endpoints = f"{user_endpoints}/{USERNAME}/graphs/{GRAPH_ID}"
+
+headers = {
+    "X-USER-TOKEN":TOKEN
 }
-
-response = requests.post(url=user_endpoints, json=parameters )
-print(response.text)
-
-graph_endpoints = f"{user_endpoints}"
 
 graph_config = {
-    "id": "graph1",
-    "name": "python100"
+    "date": "20230904",
+    "quantity": "2.5",
 }
+
+response = requests.post(url=graph_endpoints, headers=headers, json=graph_config)
+print(response.text)
+
+# --------------------------------PUT value to graph---------------------------- #
+
+# --------------------------------DELETE value to graph---------------------------- #
+
